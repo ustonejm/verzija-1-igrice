e2f234949ddedb7ac295b460cebc7720fafe1e97
boca <dunjic.boki@gmail.com>
2026-05-21 18:10:46 +0200

    Add files via upload
    verzija 1 igrice

---
+++ verzija 1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(262, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 62);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(43, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 62);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(116, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 62);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(189, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 62);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(262, 107);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(67, 62);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(262, 175);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 62);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(189, 175);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(67, 62);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(116, 175);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(67, 62);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(43, 175);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(67, 62);
+            this.button12.TabIndex = 8;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Dugme_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(378, 334);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ verzija 1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        Button prvoKliknuto = null;
+        Button drugoKliknuto = null;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InicijalizujIgru();
+        }
+
+        private void InicijalizujIgru()
+        {
+            List<int> brojevi = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+            List<Button> dugmici = new List<Button> { 
+                button1, button2, button3, button4, 
+                button5, button6, button7, button8,
+                button9, button10, button11, button12 
+            };
+
+            foreach (Button btn in dugmici)
+            {
+                int indeks = rnd.Next(0, brojevi.Count);
+                btn.Tag = brojevi[indeks];
+                btn.Text = "";
+                btn.BackColor = Control.DefaultBackColor;
+                btn.Enabled = true;
+                brojevi.RemoveAt(indeks);
+            }
+        }
+
+        private void Dugme_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled) return;
+
+            Button kliknutoDugme = sender as Button;
+            if (kliknutoDugme == null) return;
+            if (kliknutoDugme.Text != "") return;
+
+            if (prvoKliknuto == null)
+            {
+                prvoKliknuto = kliknutoDugme;
+                prvoKliknuto.Text = prvoKliknuto.Tag.ToString();
+                prvoKliknuto.BackColor = Color.Red;
+                return;
+            }
+
+            drugoKliknuto = kliknutoDugme;
+            drugoKliknuto.Text = drugoKliknuto.Tag.ToString();
+            drugoKliknuto.BackColor = Color.Red;
+
+            if (prvoKliknuto.Tag.ToString() == drugoKliknuto.Tag.ToString())
+            {
+                prvoKliknuto = null;
+                drugoKliknuto = null;
+            }
+            else
+            {
+                timer2.Start();
+            }
+        }
+
+        private void obrisi()
+        {
+            if (prvoKliknuto != null)
+            {
+                prvoKliknuto.Text = "";
+                prvoKliknuto.BackColor = Control.DefaultBackColor;
+            }
+            if (drugoKliknuto != null)
+            {
+                drugoKliknuto.Text = "";
+                drugoKliknuto.BackColor = Control.DefaultBackColor;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            obrisi();
+            prvoKliknuto = null;
+            drugoKliknuto = null;
+
+            if (timer2.Enabled)
+            {
+                timer2.Stop();
+            }
+        }
+    }
+}
